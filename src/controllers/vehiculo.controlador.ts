b250b689a96f36ae390be3967cf395ecce94ec0c
jchalare <albertchalare@gmail.com>
2022-07-15 10:32:30 -0500

    se arregla el metodo para crear vehiculo

--- src/controllers/vehiculo.controlador.ts
+++ src/controllers/vehiculo.controlador.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { Vehiculo } from '../entidades/VehiculoEntidad';
 import { AppDataSource } from "../db/db";
+import { ILike } from "typeorm";
+
 
 const dataSource = AppDataSource;
 
@@ -23,10 +25,19 @@ export const getVehiculo = async (req: Request, res: Response): Promise<Response
 };
 
 export const createVehiculos = async (req: Request, res: Response): Promise<Response> => {
-    //const {nombre, clave} = req.body
-    const nuevoVehiculo =  dataSource.getRepository(Vehiculo).create(req.body);
-    const respuesta = await dataSource.getRepository(Vehiculo).save(nuevoVehiculo);
-    return res.json(respuesta);
+    const {placa} = req.body;    
+    const vehiculoEncontrado = await dataSource.getRepository(Vehiculo).findBy({placa: ILike(`${placa}`)});
+
+    if(vehiculoEncontrado.length === 0){
+        const strPlaca = JSON.stringify(placa).toUpperCase();
+        const objPlaca = { placa:JSON.parse(strPlaca)}; 
+        const nuevoVehiculo =  dataSource.getRepository(Vehiculo).create(objPlaca);
+        const respuesta = await dataSource.getRepository(Vehiculo).save(nuevoVehiculo);
+        return res.json(respuesta);
+    }else{
+        return res.json({msg: `El vehiculo ${placa} ya existe`}); 
+    }
+
 };
 
 export const updateVehiculos = async (req: Request, res: Response): Promise<Response> => {
--- src/routes/vehiculo.rutas.ts
+++ src/routes/vehiculo.rutas.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { getVehiculos,createVehiculos,updateVehiculos,getVehiculo } from '../controllers/vehiculo.controlador';
+import { validaJwt } from '../middlewares/validarJwt';
+
 
 const rutasVehiculo = Router();
 
-rutasVehiculo.get("/vehiculo",getVehiculos);
-rutasVehiculo.get("/vehiculo/:id",getVehiculo);
-rutasVehiculo.post("/vehiculo",createVehiculos);
-rutasVehiculo.put("/vehiculo/:id",updateVehiculos);
+rutasVehiculo.get("/vehiculo",validaJwt,getVehiculos);
+rutasVehiculo.get("/vehiculo/:id",validaJwt,getVehiculo);
+rutasVehiculo.post("/vehiculo",validaJwt,createVehiculos);
+rutasVehiculo.put("/vehiculo/:id",validaJwt,updateVehiculos);
 
 
-export default rutasVehiculo;
+export default rutasVehiculo; 
